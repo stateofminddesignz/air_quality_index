09e43fa111e6b1ac0a91f81619d169433ee91aed
Larry.Dev <98300716+stateofminddesignz@users.noreply.github.com>
2023-06-29 21:44:26 +0000

    Update 3 files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="header-content">
         <img
           className="header-image"
-          src="https://www.des.nh.gov/sites/g/files/ehbemt341/files/photo-gallery/7.3-rainbow-inset.JPG"
+          src="https://images.unsplash.com/photo-1436811460420-a04ba62d66fd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80"
           alt=""
         />
         <div className="header-text">
